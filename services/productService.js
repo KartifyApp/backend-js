52bd79b3f59360227b220e5fbb10f174fd489e3b
Sartazz7 <sartajislam21@gmail.com>
2023-03-29 19:57:03 +0530

    env template

--- services/productService.js
+++ services/productService.js
@@ -11,10 +11,6 @@ export class ProductService {
             )
             return createdProduct.rowCount > 0 ? UtilityService.camelCaseObject(createdProduct.rows[0]) : null
         } catch (error) {
-            console.log(`INSERT INTO product (name, image, brand, category, description, price, stock_count, platform_id)
-            VALUES ('${product.name}', '${product.image}', '${product.brand}', '${product.category}', '${product.description}', 
-            '${product.price}', '${product.stockCount}', '${product.platformId}') RETURNING * `)
-            console.log(error)
             throw Error(`Error in creating product ${product.name}.`)
         }
     }
